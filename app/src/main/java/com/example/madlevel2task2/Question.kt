002899994304634885cb29447a268d2ea618e23b
quincyba <qbarend@gmail.com>
2020-10-23 07:02:09 +0200

    added Question.kt

---
+++ app/src/main/java/com/example/madlevel2task2/Question.kt
@@ -0,0 +1,20 @@
+package com.example.madlevel2task2
+
+import androidx.recyclerview.widget.ItemTouchHelper
+
+data class Question (
+    val question: String,
+    val answer: Int
+){
+    companion object {
+        private val correct = ItemTouchHelper.RIGHT
+        private val incorrect = ItemTouchHelper.LEFT
+        val Questions = arrayOf(
+            Question("The sky is blue", correct),
+            Question("Goldfish only have a memory of three seconds", incorrect),
+            Question("An octopus has three hearts", correct),
+            Question("Flying in an aeroplane is statistically safer than driving in a car", correct),
+            Question("The currency of France is the Franc", incorrect)
+        )
+    }
+}
